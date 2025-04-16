a51896cc06cfe681ead08a53aeea4df9e723d7ec
Floki-07 <ziyadcbz007@gmail.com>
2025-04-16 22:41:35 +0530

    Made pomodoro page responsive

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -48,7 +48,7 @@ const AppLayout = () => {
           <Route path='/calendar' element={<AuthHOC><Calendar/></AuthHOC>}/>
           <Route path='/Dates' element={<AuthHOC><Dates/></AuthHOC>}/>
           <Route path='/details' element={<Details/>}/>
-          <Route path='/pomodoro' element={<AuthHOC><Pomodoro/></AuthHOC>}/>
+          <Route path='/pomodoro' element={<Pomodoro/>}/>
           <Route path='/profile' element={<AuthHOC><Profile/></AuthHOC>}/>
           <Route path='*' element={<Error/>}/>
         </Routes>
